0266a2a006340a9dd1ea69c814ec13935b4a1c44
sohaib-mohiuddin <sohaibm131@gmail.com>
2025-02-11 22:53:21 +0000

    updated week 5

---
+++ week5/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hellohello():
+    response = {
+        "message": "Hello COMP216 W25 Class!",
+        "status": "Success"
+    }
+    
+    return jsonify(response)
+``
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5444)
